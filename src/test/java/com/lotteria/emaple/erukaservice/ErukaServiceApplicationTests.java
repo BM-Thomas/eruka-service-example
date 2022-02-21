bc7910cff186dd2ea6b16d455c7d2d4b3f1877d9
Minh Khoa <bm.hcmc.thomas@gmail.com>
2022-02-21 17:30:56 +0700

    Init

---
+++ src/test/java/com/lotteria/emaple/erukaservice/ErukaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotteria.emaple.erukaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
